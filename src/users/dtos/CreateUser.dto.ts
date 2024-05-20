8c098459c567253302532c8264e1919c69ff1cde
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2024-05-20 09:49:13 +0300

    Creating the User Entity Datatypes

---
+++ src/users/dtos/CreateUser.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  gender: string;
+
+  @IsNumber()
+  @Min(0)
+  age: number;
+}
