f9e65de79762fed2ae0a024af379dcf1ee221ffc
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2024-05-20 21:27:22 +0300

    Removing Modules

--- src/treatment/treatment.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TreatmentController } from './treatment.controller';
-import { TreatmentService } from './treatment.service';
-
-@Module({
-  controllers: [TreatmentController],
-  providers: [TreatmentService]
-})
-export class TreatmentModule {}
