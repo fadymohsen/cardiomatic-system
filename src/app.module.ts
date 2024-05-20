626c67e8a6f9018e85c1cff036f25ab8491819b2
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2024-05-20 11:43:11 +0300

    Adding New Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MedicalRecordModule } from './medical-record/medical-record.module';
+import { PrescriptionModule } from './prescription/prescription.module';
+import { DiagnosisModule } from './diagnosis/diagnosis.module';
+import { TestModule } from './test/test.module';
+import { TreatmentModule } from './treatment/treatment.module';
 
 @Module({
-  imports: [UsersModule, MedicalRecordModule],
+  imports: [UsersModule, MedicalRecordModule, PrescriptionModule, DiagnosisModule, TestModule, TreatmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
