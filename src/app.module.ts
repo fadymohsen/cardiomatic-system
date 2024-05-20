76b1f37aa83355b6ce8d6ecbb56cd49ebe18998f
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2024-05-20 11:31:17 +0300

    creating medical record module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MedicalRecordModule } from './medical-record/medical-record.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, MedicalRecordModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/medical-record/medical-record.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MedicalRecordController } from './medical-record.controller';
+import { MedicalRecordService } from './medical-record.service';
+
+@Module({
+  controllers: [MedicalRecordController],
+  providers: [MedicalRecordService]
+})
+export class MedicalRecordModule {}
