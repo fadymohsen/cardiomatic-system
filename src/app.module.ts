380672be138cf0db2e4de6a6be2575108fb4d580
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2024-05-20 17:46:00 +0300

    Adding Appointment Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PrescriptionModule } from './prescription/prescription.module';
 import { DiagnosisModule } from './diagnosis/diagnosis.module';
 import { TestModule } from './test/test.module';
 import { TreatmentModule } from './treatment/treatment.module';
+import { AppointmentModule } from './appointment/appointment.module';
 
 @Module({
-  imports: [UsersModule, MedicalRecordModule, PrescriptionModule, DiagnosisModule, TestModule, TreatmentModule],
+  imports: [UsersModule, MedicalRecordModule, PrescriptionModule, DiagnosisModule, TestModule, TreatmentModule, AppointmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
