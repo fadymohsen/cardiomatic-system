b35571149c6de8bfb5c8e2a1031436f58bcc2657
Fady Mohsen <82896079+fadymohsen@users.noreply.github.com>
2024-05-21 10:12:18 +0300

    Database Updates

---
+++ prisma/migrations/20240521063905_edit_appointment_database/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Appointment" ADD COLUMN     "doctorName" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "patientName" TEXT NOT NULL DEFAULT '';
---
+++ prisma/migrations/20240521070639_update_code_status/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Appointment" ALTER COLUMN "scheduledAt" SET DEFAULT CURRENT_TIMESTAMP;
